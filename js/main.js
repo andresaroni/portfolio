33c40f3b10c275c5247a537fbbac8ac35295c34a
andresaroni <marcoaroni10@gmail.com>
2025-07-26 14:03:29 -0500

    proyecto html

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import { cargarComponentes } from './cargarComponentes.js';
 
 cargarComponentes('header', 'components/header.html', 'styles/header.css', 'js/header.js')
 cargarComponentes('menu-bar', 'components/menu-bar.html', 'styles/menu-bar.css', 'js/menu-bar.js')
+cargarComponentes('proyectos', 'components/proyectos.html', 'styles/proyectos.css')
