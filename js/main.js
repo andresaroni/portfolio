516be43f6b49826b6435e2443d9173dd30ee0880
andresaroni <marcoaroni10@gmail.com>
2025-08-07 03:08:00 -0500

    Sección footer agregada y descripción de proyectos corregida

--- js/main.js
+++ js/main.js
@@ -3,3 +3,4 @@ import { cargarComponentes } from './cargarComponentes.js';
 cargarComponentes('header', 'components/header.html', 'js/header.js')
 cargarComponentes('menu-bar', 'components/menu-bar.html', 'js/menu-bar.js')
 cargarComponentes('proyectos', 'components/proyectos.html', 'js/proyectos.js')
+cargarComponentes('footer', 'components/footer.html')
