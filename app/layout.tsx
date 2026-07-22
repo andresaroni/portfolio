96ac0b75564feec2ddbf9e0aa74ea9e2168aee22
andresaroni <marcoaroni10@gmail.com>
2026-07-21 19:09:57 -0500

    libreria analytics de vercel agregada

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 const inter = Inter({
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased max-w-7xl mx-auto`}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
